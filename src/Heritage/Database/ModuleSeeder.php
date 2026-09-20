7735c33df07ab2e3efda9336e84e089d88c3ffc6
Muath R Abu Ouda <muathrabuouda@hotmail.com>
2026-09-20 16:25:00 +0300

    feat(database): create ModuleSeeder with automatic recursive seeder discovery

---
+++ src/Heritage/Database/ModuleSeeder.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Heritage\Database;
+
+/**
+ * Class ModuleSeeder
+ *
+ * @deprecated Use \Heritage\Database\ArtifactSeeder instead in the Ugarit Ecosystem.
+ */
+abstract class ModuleSeeder extends ArtifactSeeder
+{
+}
